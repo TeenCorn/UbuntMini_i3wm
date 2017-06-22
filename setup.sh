727df60ecf270f2c9972f83fd251b219c78992d2
TeenCorn <alexisPreeminent@hotmail.com>
2017-06-22 12:41:30 -0700

    Basic programs

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@
 
 cd ~/
 sudo apt-get update -qq			## Updates the packages so that you may install the lastest and greatest
-sudo apt-get install -yy -q xorg vim rofi feh compton alsa-utils rxvt-unicode-256color firefox scrot ranger thunar
+sudo apt-get install -yy -q xorg vim rofi feh compton alsa-utils rxvt-unicode-256color firefox scrot ranger thunar ubuntu-restricted-extras git
+
+sudo apt-get purge --auto-remove flashplugin-installer -yy
 
 ## This chuck gets the lastest stable version of i3wm on ubuntu. Because that's the only way bumblebee-status will work tehe
 sudo /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
