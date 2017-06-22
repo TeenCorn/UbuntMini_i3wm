eed9394697e3ec953c072ffa33c12d929f1d1169
TeenCorn <alexisPreeminent@hotmail.com>
2017-06-22 00:30:10 -0700

    Setup script for Ubuntu mini

--- setup.sh
+++ setup.sh
@@ -9,8 +9,7 @@ sudo apt-get install -yy xorg
 ## This chuck gets the lastest stable version of i3wm on ubuntu. Because that's the only way bumblebee-status will work tehe
 sudo /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
 sudo apt install ./keyring.deb
-sudo -i
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+sudo -i && echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 exit
 sudo apt-get update -qq
 sudo apt-get install -yy i3
