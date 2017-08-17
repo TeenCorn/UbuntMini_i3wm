207ca7631bbb838ea9581d6a764d528a7e6337aa
TeenCorn <alexisPreeminent@hotmail.com>
2017-08-16 19:53:55 -0700

    minimal install *modified 8/16/17*

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-## INSTALL SCRIPT FOR UBUNTU MINIMAL 
+## INSTALL SCRIPT FOR UBUNTU MINIMAL
 
 cd ~/
 sudo apt-get update -qq			## Updates the packages so that you may install the lastest and greatest
-sudo apt-get install -yy -q xorg vim rofi feh compton alsa-utils rxvt-unicode-256color firefox scrot ranger thunar ubuntu-restricted-extras git software-properties-common w3m mpv build-essential
+sudo apt-get install -yy -q xorg vim rofi feh compton pulseaudio firefox scrot ranger thunar ubuntu-restricted-extras git software-properties-common w3m mpv build-essential automake checkinstall lxappearance gtk-chtheme qt4-qtconfig network-manager redshift-gtk alarm-clock-applet
 
 sudo apt-get purge --auto-remove flashplugin-installer -yy
-sudo apt-get -yy install gstreamer1.0-plugins-{base,good,bad,ugly} gstreamer1.0-libav libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386		## Getting a youtube videos to work without flash
+sudo apt-get -yy install gstreamer1.0-plugins-{base,good,bad,ugly} gstreamer1.0-libav ## Getting a youtube videos to work without flash
 
 ## This chuck gets the lastest stable version of i3wm on ubuntu. Because that's the only way bumblebee-status will work tehe
 sudo /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
@@ -20,7 +20,7 @@ sudo apt-get -yy install python3.5 python3-lxml python-tox python3-pyqt5 python3
 
 sudo apt-get -yy install python-netifaces python-psutil python-requests -y 		## Dependencies needed to get Bumblebee-status running
 
-sudo pip3 install mps-youtube youtube_dl
+pip3 install mps-youtube youtube_dl
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim	## Vim plugins
 
