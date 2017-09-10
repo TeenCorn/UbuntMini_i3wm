e8994aef0b1a513f87f89c67ddfe6950fac1074a
TeenCorn <alexisPreeminent@hotmail.com>
2017-09-10 16:29:20 -0700

    i3 echo fixed

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ## INSTALL SCRIPT FOR UBUNTU MINIMAL
 
@@ -26,10 +26,11 @@ flash_delete ()
 
 i3_install ()
 {
+	clear
 	## This chuck gets the lastest stable version of i3wm on ubuntu. Because that's the only way bumblebee-status will work tehe
 	sudo /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
 	sudo apt install ./keyring.deb
-	sudo echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+	sudo su -c "echo 'deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe' >> /etc/apt/sources.list.d/sur5r-i3.list"
 	sudo apt update -qq
 	sudo apt install -yy i3
 }
