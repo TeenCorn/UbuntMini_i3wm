980ecdc6d55a89d570c867beccc422b177a240f0
TeenCorn <alexisPreeminent@hotmail.com>
2017-09-13 14:55:03 -0700

    Polybar confirmation removed

--- setup.sh
+++ setup.sh
@@ -68,7 +68,7 @@ term ()
 polybar_install ()
 {
 	cd ~/
-	sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev
+	sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev -yy
 	
 	git clone --branch 3.0.5 --recursive https://github.com/jaagr/polybar
 	mkdir polybar/build
